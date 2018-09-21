ed11f2ff9503e3e6022cdba089705bc9ed4defe9
JulianFinlando <32699627+JulianFinlando@users.noreply.github.com>
2018-09-21 22:13:23 +0700

    coding pertama

---
+++ pilihpilih/app/src/main/java/kaptenpotato/pilihpilih/StartActivity.java
@@ -0,0 +1,15 @@
+package kaptenpotato.pilihpilih;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+    }
+}
